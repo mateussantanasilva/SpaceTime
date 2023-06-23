a8367fd06501f4046449b0e86228b3d370a98816
mateussantanasilva <santanasilva1778@gmail.com>
2023-06-23 20:29:31 -0300

    :sparkles::hammer: feat: Parte servidor backend

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,129 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+
+export default async function memoriesRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', async (request) => {
+    await request.jwtVerify() // before request, check the jwt
+  })
+
+  app.get('/memories', async (request) => {
+    const memories = await prisma.memory.findMany({
+      where: {
+        userId: request.user.sub, // jwt identifier present in any request - specified in d.ts
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        content: memory.content.substring(0, 115).concat('...'), // limit amount of characters
+      }
+    })
+  })
+
+  app.get('/memories/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    if (!memory.isPublic && memory.userId !== request.user.sub)
+      return reply.status(401).send('Unauthorized for reading memory')
+
+    return memory
+  })
+
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false), // transform to boolean
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: request.user.sub,
+      },
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    let memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    if (memory.userId !== request.user.sub)
+      return reply.status(401).send('Unauthorized for reading memory')
+
+    memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+
+    return memory
+  })
+
+  app.delete('/memories/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    if (memory.userId !== request.user.sub)
+      return reply.status(401).send('Unauthorized for reading memory')
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
+}
---
+++ server/src/routes/upload.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { extname, resolve } from 'node:path'
+import { createWriteStream } from 'node:fs'
+import { pipeline } from 'node:stream'
+import { promisify } from 'node:util'
+
+const pump = promisify(pipeline)
+
+export default async function uploadRoutes(app: FastifyInstance) {
+  app.post('/upload', async (request, reply) => {
+    const uploadedFile = await request.file({
+      limits: {
+        fileSize: 5_242_880, // 5mb
+      },
+    })
+
+    if (!uploadedFile) return reply.status(400).send('File not uploaded')
+
+    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
+    const isValidFileFormat = mimeTypeRegex.test(uploadedFile.mimetype)
+
+    if (!isValidFileFormat)
+      return reply.status(400).send('File has no valid format')
+
+    const fileId = randomUUID() // create code for image
+    const extension = extname(uploadedFile.filename)
+    const fileName = fileId.concat(extension)
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads', fileName), // standardizes the path for all operating systems
+    )
+
+    await pump(uploadedFile.file, writeStream) // Streams are collections of data that are not loaded all at once.
+
+    const fullUrl = request.protocol.concat('://').concat(request.hostname)
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+
+    return fileUrl
+  })
+}
